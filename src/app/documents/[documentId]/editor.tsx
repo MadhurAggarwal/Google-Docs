4ac47c43d6bdacd5fe4367d1e2143124e37007c6
MadhurAggarwal <madhuraggarwalofficial@gmail.com>
2025-02-10 08:49:05 +0530

    Added Font-Family and Emoji

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -8,6 +8,8 @@ import Table from '@tiptap/extension-table'
 import TableCell from '@tiptap/extension-table-cell'
 import TableHeader from '@tiptap/extension-table-header'
 import TableRow from '@tiptap/extension-table-row'
+import FontFamily from '@tiptap/extension-font-family'
+import TextStyle from '@tiptap/extension-text-style';
 import Image from '@tiptap/extension-image'
 import ImageResize from "tiptap-extension-resize-image";
 import { useEditor, EditorContent } from '@tiptap/react'
@@ -50,6 +52,8 @@ export const Editor = () => {
         extensions: [
             StarterKit,
             Underline,
+            FontFamily,
+            TextStyle,
             Image,
             ImageResize,
             TaskList,
--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -1,9 +1,117 @@
 "use client";
 
 import { cn } from '@/lib/utils';
-import { BoldIcon, ItalicIcon, LucideIcon, MessageSquarePlusIcon, PrinterIcon, Redo2Icon, SpellCheckIcon, UnderlineIcon, Undo2Icon } from 'lucide-react';
+import { BoldIcon, ChevronDownIcon, ItalicIcon, ListTodoIcon, LucideIcon, MessageSquarePlusIcon, PrinterIcon, Redo2Icon, RemoveFormattingIcon, SmileIcon, SpellCheckIcon, UnderlineIcon, Undo2Icon } from 'lucide-react';
 import { useEditorStore } from '@/store/use-editor-store';
 import { Separator } from '@/components/ui/separator';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu';
+
+const FontFamilyButton = () => {
+    const {editor} = useEditorStore();
+    const fonts = [
+        {label: 'Arial', value: 'Arial'},
+        {label: 'Arial Black', value: 'Arial Black'},
+        {label: 'Arial Narrow', value: 'Arial Narrow'},
+        {label: 'Book Antiqua', value: 'Book Antiqua'},
+        {label: 'Bookman Old Style', value: 'Bookman Old Style'},
+        {label: 'Century Gothic', value: 'Century Gothic'},
+        {label: 'Comic Sans MS', value: 'Comic Sans MS'},
+        {label: 'Copperplate Gothic Bold', value: 'Copperplate Gothic Bold'},
+        {label: 'Copperplate Gothic Light', value: 'Copperplate Gothic Light'},
+        {label: 'Courier New', value: 'Courier New'},
+        {label: 'Franklin Gothic Medium', value: 'Franklin Gothic Medium'},
+        {label: 'Garamond', value: 'Garamond'},
+        {label: 'Geneva', value: 'Geneva'},
+        {label: 'Georgia', value: 'Georgia'},
+        {label: 'Helvetica', value: 'Helvetica'},
+        {label: 'Impact', value: 'Impact'},
+        {label: 'Lucida Console', value: 'Lucida Console'},
+        {label: 'Lucida Sans Unicode', value: 'Lucida Sans Unicode'},
+        {label: 'MonoSpace', value: 'MonoSpace'},
+        {label: 'Palatino Linotype', value: 'Palatino Linotype'},
+        {label: 'Tahoma', value: 'Tahoma'},
+        {label: 'Times New Roman', value: 'Times New Roman'},
+        {label: 'Trebuchet MS', value: 'Trebuchet MS'},
+        {label: 'Verdana', value: 'Verdana'},
+    ];
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <button 
+                    className='h-7 w-[120px] shrink-0 flex items-center justify-between rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm'
+                    title='Font Family'
+                >
+                    <span className='truncate'>
+                        {editor?.getAttributes('textStyle').fontFamily || 'Arial'}
+                    </span>
+                    <ChevronDownIcon className='ml-2 size-4 shrink-0'/>
+                </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className='p-1 flex flex-col gap-y-1 z-[100] max-h-60 overflow-y-auto bg-white/60 shadow-md border border-gray-300 rounded-md backdrop-blur-md'>
+                {fonts.map(({label,value}) => (
+                    <DropdownMenuItem
+                        key={value}
+                        className={cn('flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80', editor?.getAttributes('textStyle').fontFamily === value && 'bg-neutral-200/80')}
+                        style={{fontFamily: value}}
+                        onClick={() => editor?.chain().focus().setFontFamily(value).run()}
+                    >
+                        <span className='truncate text-sm'>
+                            {label}
+                            </span>
+                            
+                    </DropdownMenuItem>
+                ))}
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+};
+
+const emojis = [
+    "😂", "❤️", "🤣", "😍", "🙏", "😭", "😊", "🥰", "👍", "😁", 
+    "🔥", "😢", "🙌", "🤔", "😎", "🥹", "😃", "🎉", "👏", "💯", 
+    "🎶", "✨", "😆", "😜", "💙", "😅", "🥺", "💔", "😏", "💖", 
+    "😡", "🤩", "🤗", "💜", "🤦‍♂️", "🙈", "💛", "😇", "😋", "😌", 
+    "💪", "👀", "😤", "😝", "🤷‍♂️", "👑", "🙃", "🥳", "😔", "🤤", 
+    "😴", "🤯", "😳", "🤞", "🤙", "💚", "💗", "😑", "🎊", "🤧", 
+    "😪", "👊", "🤑", "🤡", "👻", "💀", "😈", "☠️", "🥵", "🥶", 
+    "🧐", "😠", "🤨", "🤭", "🤫", "💤", "🎁", "💃", "🕺", "👽", 
+    "🐶", "🐱", "🐼", "🦄", "🍕", "🍔", "🍟", "🌮", "🌭", "🍣", 
+    "🍪", "🍩", "🍫", "🥤", "☕", "🍷", "🍺", "🏀", "⚽", "🎸", 
+    "🎮", "🚗", "✈️", "⏳", "🔑", "📷", "📞", "📱", "💻", "🖥️", 
+    "⌚", "🎧", "📚", "📝", "🔒", "🔓", "🚀", "💎", "💡", "🔔"
+  ];
+
+const EmojiPicker = () => {
+    const { editor } = useEditorStore();
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <button 
+                    className='h-7 w-10 flex items-center justify-center rounded-sm hover:bg-neutral-200/80'
+                    title='Emoji Picker'
+                >
+                    <SmileIcon className='size-4' />
+                </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className='p-2 bg-white/60 shadow-md border border-gray-300 rounded-md backdrop-blur-md z-[100] max-h-60 overflow-y-auto'>
+                <div className="grid grid-cols-5 gap-1">
+                    {emojis.map((emoji) => (
+                        <DropdownMenuItem
+                            key={emoji}
+                            className="p-1 text-lg hover:bg-neutral-200/80 flex items-center justify-center"
+                            onClick={() => editor?.chain().focus().insertContent(emoji).run()}
+                        >
+                            {emoji}
+                        </DropdownMenuItem>
+                    ))}
+                </div>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+};
+
 interface ToolbarButtonProps {
     onClick?:  () => void;
     isActive?: boolean;
@@ -93,6 +201,17 @@ export const Toolbar = () => {
                 onClick: () => console.log('TODO: Comment'),
                 isActive: false, //TODO
             },
+            {
+                label: 'Todo List',
+                icon: ListTodoIcon,
+                onClick: () => editor?.chain().focus().toggleTaskList().run(),
+                isActive: editor?.isActive('taskList'),
+            },
+            {
+                label: 'Remove Formatting',
+                icon: RemoveFormattingIcon,
+                onClick: () => editor?.chain().focus().unsetAllMarks().run(),
+            },
         ],
     ];
 
@@ -103,7 +222,7 @@ export const Toolbar = () => {
             ))}
             
             <Separator orientation='vertical' className='h-6 bg-neutral-300'/>
-            {/* TODO: Font Family */}
+            <FontFamilyButton />
             <Separator orientation='vertical' className='h-6 bg-neutral-300'/>
             {/* TODO: Heading */}
             <Separator orientation='vertical' className='h-6 bg-neutral-300'/>
@@ -118,7 +237,7 @@ export const Toolbar = () => {
             {/* TODO: Highlight Color */}
 
             <Separator orientation='vertical' className='h-6 bg-neutral-300'/>
-
+            <EmojiPicker />
             {/* TODO: Link */}
             {/* TODO: Image */}
             {/* TODO: Align */}
